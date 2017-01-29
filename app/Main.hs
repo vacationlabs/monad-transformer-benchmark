3f076837d20455e4de26797776e48ad9bb9cbcee
Saurabh Nanda <saurabh@vacationlabs.com>
2017-01-29 12:41:19 +0530

    initial commit

---
+++ app/Main.hs
@@ -0,0 +1,82 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Main where
+
+import Lucid
+import qualified Web.Scotty.Trans as ST
+import qualified Web.Scotty as S
+import Lib
+import Data.Text.Lazy as LT
+import Control.Monad
+import Data.Text as T
+import Control.Monad.Reader
+import Network.Wreq as W
+import Control.DeepSeq
+import Control.Lens
+import Criterion.Main
+import Control.Concurrent
+
+bareScotty :: IO ()
+bareScotty = do
+  S.scotty 3000 app
+  where
+    app = S.get "/" $ do
+      S.html "hello world!"
+
+bareScottyBareLucid :: IO ()
+bareScottyBareLucid = S.scotty 3001 app
+  where
+    app = S.get "/" $ do
+      S.html $ renderText generateHtml
+
+    generateHtml = body_ $ do
+      forM [1..10000] (\_ -> div_ "hello world!")
+
+transScottyBareLucid :: IO ()
+transScottyBareLucid = ST.scottyT 3002 insideReader app
+  where
+    app :: (MonadIO m) => ST.ScottyT LT.Text m ()
+    app = ST.get "/" $ do
+      ST.html $ renderText generateHtml
+
+    generateHtml = body_ $ do
+      forM [1..10000] (\_ -> div_ "hello world!")
+
+    insideReader action = runReaderT action ()
+
+transScottyTransLucid :: IO ()
+transScottyTransLucid = ST.scottyT 3003 insideReader app
+  where
+    app :: (MonadIO m) => ST.ScottyT LT.Text m ()
+    app = ST.get "/" $ do
+      renderTextT generateHtml >>= ST.html
+
+    generateHtml :: Monad m => HtmlT m [()]
+    generateHtml = body_ $ do
+      forM [1..10000] (\_ -> div_ "hello world!")
+
+    insideReader action = runReaderT action ()
+
+fetchEndpoint port = do
+  res <- W.get ("http://localhost:" ++ (show port) ++ "/")
+  return $ force $ res ^. responseBody
+
+main :: IO ()
+main = do
+  s1 <- forkIO $ bareScotty
+  s2 <- forkIO $ bareScottyBareLucid
+  s3 <- forkIO $ transScottyBareLucid
+  s4 <- forkIO $ transScottyTransLucid
+  defaultMain
+    [
+      bench "bareScotty" $ whnfIO $ fetchEndpoint 3001
+    , bench "bareScottyBareLucid" $ whnfIO $ fetchEndpoint 3001
+    , bench "transScottyBareLucid" $ whnfIO $ fetchEndpoint 3002
+    , bench "transScottyTransLucid" $ whnfIO $ fetchEndpoint 3003
+    , bench "bareScotty" $ whnfIO $ fetchEndpoint 3001
+    ]
+  killThread s1
+  killThread s2
+  killThread s3
+  killThread s4
+  return ()
